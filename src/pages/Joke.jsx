ea9808dd674453de79c58b819ac3e157891b4463
matsixfive <maffers2006@gmail.com>
2022-05-10 22:26:39 +0100

    joke cooldown

--- src/pages/Joke.jsx
+++ src/pages/Joke.jsx
@@ -3,19 +3,29 @@ import { useState, useEffect } from "react";
 
 export default function Joke() {
   const [jokeText, setJokeText] = useState("");
+  const [jokeAvaliable, setJokeAvaliable] = useState(true);
 
   let getJoke = () => {
-    axios
-      .get("https://icanhazdadjoke.com", { headers: { Accept: "text/plain" } })
-      .then((res) => {
-        setJokeText(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    console.log(jokeAvaliable);
+    if (jokeAvaliable) {
+      axios
+        .get("https://icanhazdadjoke.com", {
+          headers: { Accept: "text/plain" },
+        })
+        .then((res) => {
+          setJokeText(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+          if (err.response.status === 429) {
+            setJokeText("Too many jokes! Try waiting a minute.");
+            setJokeAvaliable(false);
+            setTimeout(setJokeAvaliable(true), 60000);
+          }
+        });
+    }
   };
 
-  
   useEffect(() => {
     getJoke();
   }, []);
@@ -23,6 +33,7 @@ export default function Joke() {
   return (
     <>
       <p>{jokeText}</p>
+      <button onClick={getJoke}>Get Joke</button>
     </>
   );
 }
