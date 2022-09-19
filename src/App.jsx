8bd83b38e34d7752f74e395480e7ffb220072774
matsixfive <maffers2006@gmail.com>
2022-09-19 15:53:35 +0100

    all cleaned up

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
-import devTools from "devtools-detect";
-
-import Header from "./headfoot/Header";
-import Footer from "./headfoot/Footer";
-
-import PageNotFound from "./pages/404/PageNotFound";
-
-import Homepage from "./pages/homepage/Homepage";
-import Joke from "./pages/Joke";
-import IP from "./pages/IP";
+import Homepage from "./Homepage";
 
 export default function App() {
-  //* Check if devtools are open (useless)
-  const devT = () => {
-    console.log("Hello developer :)");
-  };
-  useEffect(() => {
-    if (devTools.isOpen) {
-      devT();
-    }
-    window.addEventListener("devtoolschange", (event) => {
-      if (event.detail.isOpen) {
-        devT();
-      }
-    });
-  }, []);
-
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          {/* All base level pages */}
-          <Route index element={<Homepage />} />
-          <Route path="head" element={<Header />} />
-          <Route path="foot" element={<Footer />} />
-          <Route path="joke" element={<Joke />} />
-          <Route path="ip" element={<IP />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return <Homepage />;
 }
---
+++ src/Homepage.jsx
@@ -0,0 +1,141 @@
+import { useEffect, useState, useRef } from "react";
+
+import AgeSection from "./AgeSection";
+import Footer from "./Footer";
+
+import styles from "./homepage.module.scss";
+
+import {findDivisions} from "../lib/findDivisions";
+
+export default function Homepage() {
+	const [canShow, setCanShow] = useState(false);
+	const [formattedAge, setFormattedAge] = useState({
+		years: 0,
+		days: 0,
+		hours: 0,
+		minutes: 0,
+		seconds: 0,
+		milliseconds: 0,
+	});
+	const [yearsLeft, setYearsLeft] = useState();
+
+	const [showDateSelect, setShowDateSelect] = useState(false);
+	const [targetAge, setTargetAge] = useState(18);
+
+	
+
+	// 31/01/2006 03:02:00 in epoch time
+	const myBirthday = 1138676554936;
+	const [birthday, setBirthday] = useState({
+		date: myBirthday,
+		time: 0,
+		dateTime: myBirthday,
+	});
+
+	const epochToDays = 86400000;
+	const oneYearMs = 31557600000;
+
+	const fps = 53; // odd number gives more random ms digits
+
+	let birthdayRenderer = useRef();
+
+	useEffect(() => {
+		clearInterval(birthdayRenderer.current);
+		setCanShow(true);
+
+		birthdayRenderer.current = setInterval(() => {
+			const epochAge = Date.now() - birthday.dateTime;
+
+			const divisions = findDivisions(epochAge);
+			setFormattedAge(divisions);
+
+			const targetYears =
+				divisions.years < 0
+					? 0
+					: divisions.years < 18
+					? 18
+					: Math.ceil(divisions.years / 10) * 10;
+			setTargetAge(targetYears);
+
+			// 18 years in days - current age in days
+			// one year is 31557600000 ms
+			setYearsLeft(
+				(
+					(oneYearMs * targetYears) / epochToDays -
+					epochAge / epochToDays
+				).toFixed(6)
+			);
+		}, 1000 / fps);
+	}, [birthday.dateTime]);
+	return (
+		<div style={{ minHeight: "100vh", position: "relative" }}>
+			{canShow ? (
+				<main className={styles.container}>
+					{`${birthday.dateTime === myBirthday ? "I'm" : "You're"} `}
+					{birthday.dateTime > Date.now() ? (
+						" not born yet..."
+					) : (
+						<>
+							<AgeSection
+								age={formattedAge.years}
+								timePeriod={"year"}
+								styleName={styles.timeYear}
+								singular
+							/>
+							{", "}
+							<AgeSection
+								age={formattedAge.days}
+								timePeriod={"day"}
+								styleName={styles.timeDay}
+								singular
+							/>
+							{", "}
+							<AgeSection
+								age={formattedAge.hours}
+								timePeriod={"hour"}
+								styleName={styles.timeHour}
+								singular
+							/>
+							{", "}
+							<AgeSection
+								age={formattedAge.minutes}
+								timePeriod={"minute"}
+								styleName={styles.timeMinute}
+								singular
+							/>
+							{", "}
+							<AgeSection
+								age={formattedAge.seconds}
+								timePeriod={"second"}
+								styleName={styles.timeSecond}
+								singular
+								chars={2}
+							/>
+							{", and "}
+							<AgeSection
+								age={formattedAge.milliseconds}
+								timePeriod={"millisecond"}
+								styleName={styles.timeMillisecond}
+								chars={3}
+							/>
+							{" old."}
+						</>
+					)}
+					{" There are "}
+					<span className={styles.timeLeft.concat(` ${styles.time}`)}>
+						{yearsLeft}
+					</span>
+					{` days until ${
+						birthday.dateTime === myBirthday ? "I'm" : "you're"
+					} ${targetAge !== 0 ? targetAge + " years old" : " born"}.`}
+				</main>
+			) : null}
+			<Footer
+				showDateSelect={showDateSelect}
+				setShowDateSelect={setShowDateSelect}
+				birthday={birthday}
+				setBirthday={setBirthday}
+			/>
+		</div>
+	);
+}
