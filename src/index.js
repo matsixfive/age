41168a9f5c1c49023268a135507634524439cdea
matsixfive <maffers2006@gmail.com>
2022-11-11 11:52:28 +0000

    vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import {inject} from "@vercel/analytics";
+
+inject()
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
